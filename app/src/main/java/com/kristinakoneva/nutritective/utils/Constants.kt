5ae739ebc0be77d769e5eea34cdb8a009897f88e
Kristina Koneva <kristina.koneva@infinum.com>
2024-04-08 15:51:24 +0200

    Update OpenFoodFacts API version

--- app/src/main/java/com/kristinakoneva/nutritective/utils/Constants.kt
+++ app/src/main/java/com/kristinakoneva/nutritective/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.kristinakoneva.nutritective.utils
 
 object Constants {
 
-    const val OPEN_FOOD_FACTS_API_BASE_URL = "https://world.openfoodfacts.org/api/v0/"
+    const val OPEN_FOOD_FACTS_API_BASE_URL = "https://world.openfoodfacts.org/api/v2/"
     const val CALORIES_NINJAS_API_BASE_URL = "https://api.calorieninjas.com/v1/"
     const val CALORIES_NINJA_API_KEY_HEADER = "X-Api-Key"
 }
