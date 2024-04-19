8c9efdf5d74cc422e85e9d3aa1caba89a0203dd8
Kristina Koneva <kristina.koneva@infinum.com>
2024-04-19 22:05:27 +0200

    Minor changes

--- app/src/main/java/com/kristinakoneva/nutritective/ui/shared/composables/BottomNavigationBar.kt
+++ app/src/main/java/com/kristinakoneva/nutritective/ui/shared/composables/BottomNavigationBar.kt
@@ -31,8 +31,11 @@ fun BottomNavigationBar(navController: NavController) {
                     selected = currentRoute == item.route,
                     onClick = {
                         navController.navigate(item.route) {
-                            popUpTo(navController.graph.startDestinationId)
+                            popUpTo(navController.graph.startDestinationId){
+                                saveState = true
+                            }
                             launchSingleTop = true
+                            restoreState = true
                         }
                     },
                     icon = { Icon(item.icon, contentDescription = stringResource(item.label)) },
